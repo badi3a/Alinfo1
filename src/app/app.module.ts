60eb088b3e8c91d1456c1d81bb11b3dbdffca07a
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2022-04-22 11:10:25 +0200

    dataBinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ListProductComponent } from './list-product/list-product.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { ListProductComponent } from './list-product/list-product.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/list-product/list-product.component.ts
+++ src/app/list-product/list-product.component.ts
@@ -9,6 +9,7 @@ import {Product} from "../model/product";
 export class ListProductComponent implements OnInit {
   public title: string;
   public list: Product[];
+  public priceMax:number;
   constructor() { }
   ngOnInit():void {
     this.title= 'Eshop';
@@ -17,21 +18,21 @@ export class ListProductComponent implements OnInit {
         title: 'T-shirt1',
         quantity: 10,
         like: 2,
-        price: '120',
+        price: 120,
         picture: 'https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F49%2F56%2F49560dfa45732ed844e8e4742afdca248ee60f9f.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_tshirtstanks_shortsleeve%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]'
       },
       { id: 12,
         title: 'T-shirt 2',
         quantity: 0,
         like: 3,
-        price: '14',
+        price: 14,
         picture: 'https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F49%2F56%2F49560dfa45732ed844e8e4742afdca248ee60f9f.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_tshirtstanks_shortsleeve%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]'
       },
       { id: 34,
         title: 'T-shirt 4',
         quantity: 2,
         like: 123,
-        price: '17',
+        price: 17,
         picture: 'https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F49%2F56%2F49560dfa45732ed844e8e4742afdca248ee60f9f.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_tshirtstanks_shortsleeve%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]'
       }
     ];
